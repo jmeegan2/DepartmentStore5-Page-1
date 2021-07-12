272b28233b75e077227d4ab3ce5caff6f1a52226
James Meegan <jmeegan2@owlmail.harford.edu>
2021-07-12 18:34:45 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        long itemID = 0;
+        double price = 0.00d;
+        String description = "Unknown";
+        System.out.println("Item Identification");
+        System.out.println("Item Number:" + itemID);
+        System.out.println("Description: " + description);
+        System.out.printf("Unit Price: %.2f\n", price);
+    }
+}
